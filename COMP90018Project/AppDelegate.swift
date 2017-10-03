e295acead6b16b21fd3146648231221c73380c5a
xuelinzhao <zhaoxuelin1994@gmail.com>
2017-10-04 00:43:00 +1100

    Info.plist Update

--- COMP90018Project/AppDelegate.swift
+++ COMP90018Project/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSServices.provideAPIKey("AIzaSyCvHBeL1YQeuWLAaBBfCbsY1zO7l4V0pL0")
+       GMSServices.provideAPIKey("AIzaSyCvHBeL1YQeuWLAaBBfCbsY1zO7l4V0pL0")
         return true
     }
 
