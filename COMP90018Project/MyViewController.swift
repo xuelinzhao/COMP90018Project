54ee90fe8755404288a7dde47d2333dc8f7fcf0c
shaneg26 <shaneg26@live.com>
2017-10-04 15:50:29 +1100

    Create Tools View & MyView controller

---
+++ COMP90018Project/MyViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MyViewController.swift
+//  COMP90018Project
+//
+//  Created by 郭圣聪 on 4/10/17.
+//  Copyright © 2017 Microsoft. All rights reserved.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+    
+    @IBOutlet weak var buttonSOS: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func sosPressed(_ sender: Any) {
+    }
+    
+}
